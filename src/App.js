0cfeceab44bca14d33c7bdc2ae836ab58a002fa8
11m245 <sivaraj2siva@gmail.com>
2022-11-27 09:45:54 +0530

    last_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
 function App() {
+
+  const free = { name: "free", price: 0, features: [{ status: "enabled", feature: "Single User" }, { status: "enabled", feature: "5GB Storage" }, { status: "enabled", status: "enabled", feature: "Unlimited Public Projects" }, { status: "enabled", feature: "Community Access" }, { status: "disabled", feature: "Unlimited Private Projects" }, { status: "disabled", feature: "Dedicated Phone Support" }, { status: "disabled", feature: "Free Subdomain" }, { status: "disabled", feature: "Monthly Status Reports" }] };
+  const plus = { name: "plus", price: 9, features: [{ status: "enabled", feature: "5 Users" }, { status: "enabled", feature: "5GB Storage" }, { status: "enabled", status: "enabled", feature: "Unlimited Public Projects" }, { status: "enabled", feature: "Community Access" }, { status: "enabled", feature: "Unlimited Private Projects" }, { status: "enabled", feature: "Dedicated Phone Support" }, { status: "enabled", feature: "Free Subdomain" }, { status: "disabled", feature: "Monthly Status Reports" }] };
+  const pro = { name: "pro", price: 49, features: [{ status: "enabled", feature: "Unlimited Users" }, { status: "enabled", feature: "5GB Storage" }, { status: "enabled", status: "enabled", feature: "Unlimited Public Projects" }, { status: "enabled", feature: "Community Access" }, { status: "enabled", feature: "Unlimited Private Projects" }, { status: "enabled", feature: "Dedicated Phone Support" }, { status: "enabled", feature: "Unlimited Free Subdomain" }, { status: "enabled", feature: "Monthly Status Reports" }] };;
+  const cards = [free, plus, pro];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="price-cards-wrapper">
+
+      {cards.map((variant, index) => (<Card key={index} variant={variant} />))}
+      {/* <Card variant={free} />
+      <Card variant={plus} />
+      <Card variant={pro} /> */}
+    </div>
+  );
+}
+
+function Card({ variant, index }) {
+  console.log(variant);
+  let { name, price, features } = variant;
+
+  return (
+    <div key={index} className="card">
+      <div className="price-w">
+        <h3 className="variant">{name}</h3>
+        <p className='price-value'> &#8377; {price} <span>/month</span></p>
+      </div>
+      <div className="options">
+        {features.map((fobj, index) => (<Feature key={index} status={fobj.status} option_name={fobj.feature} />))}
+      </div>
+      <button className='button-variant'>BUTTON</button>
+    </div>
+
+  );
+
+}
+
+function Feature({ status, option_name, index }) {
+
+  const image_url = (status === "enabled") ? ("./check.png") : ("./close.png");
+  const disabled_image_style = { filter: "opacity(0.4)" };
+  const disabled_text_style = { color: "grey" };
+  const option_image_style = (status === "disabled") ? disabled_image_style : null;
+  const option_text_style = (status === "disabled") ? disabled_text_style : null;
+
+  console.log(status, option_name);
+
+  return (
+    <div key={index} className="option-wrapper">
+      <img style={option_image_style} className='option-image' src={image_url} alt="" />
+      <p style={option_text_style} className='option-name'>{option_name}</p>
     </div>
   );
 }
